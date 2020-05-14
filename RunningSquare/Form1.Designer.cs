dcb7e163dcc29385f7c0a404aa87646697a33312
wojciech w <mcwwphd@gmail.com>
2020-05-14 23:41:12 +0200

    Add project files.

---
+++ RunningSquare/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace RunningSquare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlBox = new System.Windows.Forms.Panel();
+            this.timerTicker = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // pnlBox
+            // 
+            this.pnlBox.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pnlBox.Location = new System.Drawing.Point(373, 221);
+            this.pnlBox.Name = "pnlBox";
+            this.pnlBox.Size = new System.Drawing.Size(103, 103);
+            this.pnlBox.TabIndex = 0;
+            // 
+            // timerTicker
+            // 
+            this.timerTicker.Enabled = true;
+            this.timerTicker.Interval = 150;
+            this.timerTicker.Tick += new System.EventHandler(this.timerTicker_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 576);
+            this.Controls.Add(this.pnlBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timerTicker;
+        public System.Windows.Forms.Panel pnlBox;
+    }
+}
+
---
+++ RunningSquare/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RunningSquare
+{
+    /*Panel skaczący losowo na ekranie i zmieniający kolor co 150ms.
+Delta pozycji X i Y wynosi od -75 do 75.*/
+
+    public partial class Form1 : Form
+    {
+        int maxX, maxY;
+        Random random = new Random((int)DateTime.Now.Ticks);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void timerTicker_Tick(object sender, EventArgs e)
+        {
+            if (pnlBox.Left > 0 && pnlBox.Right < maxX && pnlBox.Top < maxY && pnlBox.Bottom > 0)
+            {
+
+                int addX = pnlBox.Location.X + random.Next(-75, 75);
+                int addY = pnlBox.Location.Y + random.Next(-75, 75);
+
+                pnlBox.Location = new Point(addX, addY);
+            }
+          
+
+            byte R = (byte)random.Next(0, 255);
+            byte G = (byte)random.Next(0, 255);
+            byte B = (byte)random.Next(0, 255);
+
+            pnlBox.BackColor = Color.FromArgb(R, G, B);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form form = (Form)sender;
+            maxX = form.Size.Width;
+            maxY = form.Size.Height;
+
+        }
+    }
+}
